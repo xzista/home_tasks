30b4897dfc9b58c53e31d84bca6caae1633dce53
xziste <17alexdmz99@gmail.com>
2025-03-27 22:53:00 +0300

    decided task 4.1 in lesson 11.2

---
+++ _11_2_tasks/src/task_4_1.py
@@ -0,0 +1,29 @@
+"""Напишите декоратор, который проверяет,
+ что все числа, возвращаемые декорируемой функцией,
+ являются целыми, и округляет их до целых, если это не так."""
+
+def decorate_check_integers(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        if type(result) == int:
+            return result
+        elif type(result) in [list, tuple]:
+            return type(result), [round(x) if type(x) == float else x for x in result]
+        elif type(result) == float:
+            return round(result)
+        elif type(result) == str:
+            if '.' in result:
+                new_result = result.replace('.', '')
+                if new_result.isdigit():
+                    return type(result), str(round(float(result)))
+            else:
+                return result
+        else:
+            return result
+    return wrapper
+
+@decorate_check_integers
+def numbers():
+    return ['12.6', 12.2, 12.6]
+
+print(numbers())
