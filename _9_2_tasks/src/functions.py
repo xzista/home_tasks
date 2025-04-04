6f8d2abaf0a905c79dff4c7e6c297a2686674496
xziste <17alexdmz99@gmail.com>
2025-04-04 19:52:10 +0300

    decided task 3 in lesson 9.2

--- _9_2_tasks/src/functions.py
+++ _9_2_tasks/src/functions.py
@@ -7,11 +7,14 @@
 Сделайте новый коммит.
 В отдельном коммите измените TXT-файл, сделав его меньше для простоты тестирования."""
 import os
+import re
+
+CURRENT_DIR = os.path.dirname(__file__)
 
 
 def name_str_from_file(file: str) -> list:
-    current_dir = os.path.dirname(__file__)
-    file_path = os.path.join(current_dir, "../data/" + file)
+    """Функция очищения имен из файла и возвращения их в список"""
+    file_path = os.path.join(CURRENT_DIR, "../data/" + file)
     with open(file_path, 'r', encoding='utf-8') as f:
         name_list = f.readlines()
     new_list = []
@@ -28,3 +31,26 @@ def name_str_from_file(file: str) -> list:
 
 if __name__ == '__main__':
     print(name_str_from_file('names.txt'))
+
+"""Напишите другие функции, позволяющие из списка имен получать только русские и английские 
+и записывать эти имена в файл в отсортированном виде. 
+В итоге программа должна считывать данные из исходного файла и формировать два файла 
+с русскими и английскими именами, отсортированными по алфавиту. 
+Новые файлы должны создаваться в папке data/. 
+Всегда делайте точечные коммиты. После завершения разработки сделайте отмену коммита, 
+в котором был урезан исходный файл, т. е. верните полный TXT-файл."""
+
+
+def split_lang(list_names: list) -> None:
+    file_path = os.path.join(CURRENT_DIR, "../data/")
+    str_from_list = ' '.join(list_names)
+    eng_names = re.findall(r'\w+',str_from_list , flags=re.ASCII)
+    rus_names = list(set(list_names).difference(set(eng_names)))
+    with open(file_path + 'rus_names.txt', 'w', encoding='utf-8') as file_ru:
+        file_ru.write('\n'.join(sorted(rus_names)))
+    with open(file_path + 'eng_names.txt', 'w', encoding='utf-8') as file_en:
+        file_en.write('\n'.join(sorted(eng_names)))
+
+
+if __name__ == '__main__':
+        split_lang(name_str_from_file('names.txt'))
