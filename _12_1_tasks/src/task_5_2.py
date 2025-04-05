fe0c20d370c5927cfdae527fdb6079e958c3d0a4
xziste <17alexdmz99@gmail.com>
2025-04-05 04:39:31 +0300

    add decorator @log_stat in task 5.2 in lesson 12.1

--- _12_1_tasks/src/task_5_2.py
+++ _12_1_tasks/src/task_5_2.py
@@ -10,6 +10,19 @@
 import json
 
 
+def log_stat(func):
+    """Декоратор, который будет выводить в консоль статистику
+по количеству отфильтрованных транзакций"""
+    def wrapper(*args, **kwargs):
+        result_func = func(*args, **kwargs)
+        count = len(result_func)
+        sum_price = [abs(price['amount']) for price in result_func]
+        print('Абсолютная суммарная стоимость: ', sum(sum_price))
+        print('Количество отфильтрованных транзакций: ', count)
+    return wrapper
+
+
+@log_stat
 def filtered_transactions(input_file, output_file, filter_by_currency='USD'):
     """Функция фильтрует данные в JSON файле по валюте, и сохраняет в новый файл результат"""
     with open('../data/' + input_file) as file:
@@ -17,7 +30,7 @@ def filtered_transactions(input_file, output_file, filter_by_currency='USD'):
     filtered_data = [data for data in dict_data if data['currency'] == filter_by_currency]
     with open('../data/' + output_file, 'w') as file:
         json.dump(filtered_data, file, indent=4)
-    return
+    return filtered_data
 
 
 if __name__ == '__main__':
